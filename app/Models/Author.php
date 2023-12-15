afd717b53966fc00813f1e1b81b1a40333be2745
John Nasimba <nasimba4john@gmail.com>
2023-12-15 10:08:32 +0200

    define models

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'author_name',
+        'author_contact_no',
+        'author_country',
+        'created_at',
+        'updated_at'
+    ];
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'book_title',
+        'book_isbn',
+        'book_price',
+        'book_publish_year',
+        'author_id',
+        'created_at',
+        'updated_at'
+    ];
 }
